b7d1b909105fc272aeae5fd7258674d9604fe7ae
pfuller <pfuller@360view.com>
2020-03-09 13:16:00 -0700

    remove file

--- .huskyrc.js
+++
@@ -1,5 +0,0 @@
-// @ts-check
-const { commitHooks, generateHuskyConfig } = require('./.config/standards/husky/husky.js');
-const huskyHooks = generateHuskyConfig(commitHooks());
-console.log(huskyHooks);
-module.exports = huskyHooks;
