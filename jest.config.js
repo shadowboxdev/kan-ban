689a27d3f468644e63323d99ad8a3af0611ec289
paul filler <shadowboxlinux@gmail.com>
2020-03-09 13:26:24 -0700

    testing commit hooks

--- jest.config.js
+++ jest.config.js
@@ -7,15 +7,17 @@ module.exports = {
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png)$': '<rootDir>/__mocks__/image.js',
-    '^@lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^@core/(.*)$': '<rootDir>/projects/core/src/lib/$1',
+    '^@common/(.*)$': '<rootDir>/projects/common/src/lib/$1',
+    '^@app/(.*)$': '<rootDir>/src/$1'
   },
   setupFilesAfterEnv: ['<rootDir>/.config/testing/setup-jest.ts'],
   transform: {
     '^.+\\.(ts|html)$': 'ts-jest',
-    '^.+\\.js$': 'babel-jest',
-  },
+    '^.+\\.js$': 'babel-jest'
+  }
 };
