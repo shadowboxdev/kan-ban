bda56696a7f295934690ed7da037ae5eaffc496e
paul filler <shadowboxlinux@gmail.com>
2020-03-09 13:45:21 -0700

    update

---
+++ .huskyrc.js
@@ -0,0 +1,5 @@
+// @ts-check
+const { covalentHooks, generateHuskyConfig } = require('./.config/standards/husky/husky.js');
+const huskyHooks = generateHuskyConfig(covalentHooks());
+
+module.exports = huskyHooks;
